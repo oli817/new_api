7e92f18f7c44d15026bde282804b83697c2e1cca
oli817 <63024938+oli817@users.noreply.github.com>
2021-11-24 16:13:05 -0800

    yes

--- Receiver/app.py
+++ Receiver/app.py
@@ -11,8 +11,8 @@ import os
 
 if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
     print("In Test Environment")
-    app_conf_file = "/config/app_conf.yml"
-    log_conf_file = "/config/log_conf.yml"
+    app_conf_file = "/config/receiver/app_conf.yml"
+    log_conf_file = "/config/receiver/log_conf.yml"
 else:
     print("In Dev Environment")
     app_conf_file = "app_conf.yml"
